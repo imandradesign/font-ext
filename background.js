626a7acad9c0786d0dce2f7f88f2d4b52d5b3094
Deb <55760860+dmargarella@users.noreply.github.com>
2019-10-23 11:42:39 -0700

    Create background.js
    Background script

---
+++ background.js
@@ -0,0 +1,10 @@
+{
+    "name": "Adobe Font Information Extension",
+    "version": "1.0",
+    "description": "Hover over a font on a Squarespace site to see information about the font.",
+    "background": {
+      "scripts": ["background.js"],
+      "persistent": false
+    },
+    "manifest_version": 2
+  }
